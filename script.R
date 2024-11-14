816fe7481d04fb25ecf9a16d9d7db9ac41f0f748
lgaliana <lino.galiana@insee.fr>
2024-11-14 11:08:46 +0000

    Finalise l'application 4

--- script.R
+++ script.R
@@ -1,72 +1,121 @@
-library(MASS)
+# ENVIRONNEMENT -------------------------------
+
 library(dplyr)
 library(ggplot2)
-library(forcats)
 
 api_token <- "trotskitueleski$1917"
 
-# j'importe les données avec read_csv2 parce que c'est un csv avec des ; et que read_csv attend comme separateur des , 
-df <- readr::read_csv2(
-  "individu_reg.csv",
-  col_select = c("region", "aemm", "aged", "anai","catl","cs1", "cs2", "cs3", "couple", "na38", "naf08", "pnai12", "sexe", "surf", "tp", "trans", "ur"))
 
-df <- df %>%
-  mutate(aged = as.numeric(aged))
+# FONCTIONS ==================================
 
-df %>% group_by(aged) %>% summarise(n())
+decennie_a_partir_annee <- function(annee) return(annee - annee %% 10)
 
-decennie_a_partir_annee    = function(ANNEE){ return(ANNEE - ANNEE %%
-                                                       10) }
-
-ggplot(df) + geom_histogram(aes(x = 5*floor(as.numeric(aged)/5)), stat = "count")
-
-# correction (qu'il faudra retirer)
-# ggplot(
-#   df %>% group_by(aged, sexe) %>% summarise(SH_sexe = n()) %>% group_by(aged) %>% mutate(SH_sexe = SH_sexe/sum(SH_sexe)) %>% filter(sexe==1)
-# ) + geom_bar(aes(x = as.numeric(aged), y = SH_sexe), stat="identity") + geom_point(aes(x = as.numeric(aged), y = SH_sexe), stat="identity", color = "red") + coord_cartesian(c(0,100))
-
-# stats trans par statut
-df3 = df %>% group_by(couple, trans) %>% summarise(x = n()) %>% group_by(couple) %>% mutate(y = 100*x/sum(x))
-
-p <- # part d'homme dans chaque cohort
-  df %>% 
-  group_by(aged, sexe) %>% 
-  summarise(SH_sexe = n()) %>% 
-  group_by(aged) %>% 
-  mutate(SH_sexe = SH_sexe/sum(SH_sexe)) %>% 
-  filter(sexe==1) %>%
-  ggplot() + geom_bar(aes(x = aged, y = SH_sexe), stat="identity") + geom_point(aes(x = aged, y = SH_sexe), stat="identity", color = "red") + coord_cartesian(c(0,100))
-
-
-ggsave("p.png", p)
-
-df <- df %>% mutate(sexe = as.character(sexe)) %>%
-  mutate(sexe = fct_recode(sexe, Homme = "1", Femme = "2"))
-
-#fonction de stat agregee
-fonction_de_stat_agregee<-function(a,b="moyenne",...){
-  if (b=="moyenne"){
-    x=mean(a, na.rm = T,...)
-  } else if (b=="ecart-type" | b == "sd"){
-    x = sd(a, na.rm = T, ...)
-  } else if (b=="variance"){
-    x = var(a, na.rm = T, ...)
+#' Fonction de Statistique Agrégée
+#'
+#' Calcule une statistique agrégée (moyenne, écart-type ou variance) pour un vecteur numérique donné.
+#'
+#' @param a Un vecteur numérique de valeurs pour lequel la statistique agrégée sera calculée.
+#' @param b Une chaîne de caractères spécifiant le type de statistique à calculer. Options possibles :
+#'   \itemize{
+#'     \item `"moyenne"` : Calcule la moyenne (par défaut).
+#'     \item `"ecart-type"` ou `"sd"` : Calcule l'écart-type.
+#'     \item `"variance"` : Calcule la variance.
+#'   }
+#' @param ... Arguments supplémentaires passés aux fonctions sous-jacentes (par exemple, `mean`, `sd`, `var`).
+#'
+#' @return Une valeur numérique représentant la statistique calculée.
+#'
+#' @examples
+#' # Calculer la moyenne d'un vecteur normal aléatoire
+#' fonction_de_stat_agregee(rnorm(10))
+#'
+#' # Calculer l'écart-type d'un vecteur normal aléatoire
+#' fonction_de_stat_agregee(rnorm(10), "ecart-type")
+#'
+#' # Calculer la variance d'un vecteur normal aléatoire
+#' fonction_de_stat_agregee(rnorm(10), "variance")
+#'
+#' @export
+fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
+  
+  if (b == "moyenne") {
+    x <- mean(a, na.rm = TRUE, ...)
+  } else if (b == "ecart-type" || b == "sd") {
+    x <- sd(a, na.rm = TRUE, ...)
+  } else if (b == "variance") {
+    x <- var(a, na.rm = TRUE, ...)
   }
   return(x)
+  
 }
 
-fonction_de_stat_agregee(rnorm(10))
-fonction_de_stat_agregee(rnorm(10), "ecart-type")
-fonction_de_stat_agregee(rnorm(10), "variance")
+
+# IMPORT --------------------------------------------
+
+df <- readr::read_csv2(
+  "individu_reg.csv",
+  col_select = c(
+    "region", "aemm", "aged", "anai", "catl",
+    "cs1", "cs2", "cs3", "couple", "na38", "naf08",
+    "pnai12", "sexe", "surf", "tp", "trans", "ur"
+  )
+)
+
+
+# NETTOYAGES --------------------------------------------
+
+df <- df %>%
+  mutate(aged = as.numeric(aged)) %>%
+  mutate(sexe = as.character(sexe)) %>%
+  mutate(sexe = forcats::fct_recode(sexe, Homme = "1", Femme = "2"))
+
+
+stats_transport_age <- df %>%
+  group_by(couple, trans) %>%
+  summarise(x = n()) %>%
+  group_by(couple) %>%
+  mutate(y = 100 * x / sum(x))
+
+
+part_hommes_cohortes <- df %>%
+  group_by(aged, sexe) %>%
+  summarise(SH_sexe = n()) %>%
+  group_by(aged) %>%
+  mutate(SH_sexe = SH_sexe / sum(SH_sexe)) %>%
+  filter(sexe == 1)
+
+
+# STATISTIQUES DESCRIPTIVES ------------------------------
+
+# STATISTIQUES UNIVARIEES ============================
 
 fonction_de_stat_agregee(df %>% filter(sexe == "Homme") %>% pull(aged))
 fonction_de_stat_agregee(df %>% filter(sexe == "Femme") %>% pull(aged))
 
 
-# modelisation
-df3=df%>%select(surf,cs1,ur,couple,aged)%>%filter(surf!="Z")
-df3[,1]=factor(df3$surf, ordered = T)
-df3[,"cs1"]=factor(df3$cs1)
-df3 %>% 
-  filter(couple == "2" & aged>40 & aged<60)
-polr(surf ~ cs1 + factor(ur), df3)
+# DISTRIBUTION D'AGE =================================
+
+ggplot(df) +
+  geom_histogram(aes(x = 5 * floor(as.numeric(aged) / 5)), stat = "count")
+
+
+p <- part_hommes_cohortes %>%
+  ggplot() +
+  geom_bar(aes(x = aged, y = SH_sexe), stat = "identity") +
+  geom_point(aes(x = aged, y = SH_sexe), stat = "identity", color = "red") +
+  coord_cartesian(c(0, 100))
+
+p
+ggsave("p.png", p)
+
+
+
+
+# MODELISATION ----------------------------------------
+
+echantillon_modelisation <- df %>%
+  select(surf, cs1, ur, couple, aged) %>%
+  filter(surf != "Z") %>%
+  mutate(surf = factor(surf, ordered = TRUE), cs1 = factor(cs1))
+
+MASS::polr(surf ~ cs1 + factor(ur), echantillon_modelisation)
